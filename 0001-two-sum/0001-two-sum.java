521012d94e5c32aa6f87508b5b71ec3cb89db879
ramakanthreddykonda90-lang <ramakanthreddykonda90@gmail.com>
2026-03-12 15:55:53 +0530

    Time: 45 ms (27.06%), Space: 47.1 MB (70.67%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int idx1=-1;
+        int idx2=-1;
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    idx1=i;
+                    idx2=j;
+                    break;
+                }
+                if(idx1!=-1)
+                break;
+            }
+        }
+        return new int[]{idx1,idx2};
+    }
+}
